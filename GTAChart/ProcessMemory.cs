bf225cc9a9735704eb6c93f965168bc675a3f57c
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-05-01 22:23:03 +0200

    chore: process memory implementation

---
+++ GTAChart/ProcessMemory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GTAChart
+{
+	public class ProcessMemory
+	{
+		private IntPtr Handle { get; set; }
+
+		public ProcessMemory(ulong pId, ProcessAccessRights accessRights)
+		{
+			OpenProcess(pId, accessRights);
+		}
+
+		public ProcessMemory(ulong pId) : this(pId, ProcessAccessRights.All)
+		{
+		}
+
+		public void OpenProcess(ulong pId, ProcessAccessRights access)
+		{
+			Handle = Memory.OpenProcess(access.Value, false, pId);
+			if (Handle == IntPtr.Zero)
+				throw new AccessViolationException("No permission to access the process with ID " + pId + ".");
+		}
+
+		public bool Write<T>(IntPtr address, T data) where T : struct
+		{
+			return Memory.Write(Handle, address, data);
+		}
+
+		public T Read<T>(IntPtr address) where T : struct
+		{
+			return Memory.Read<T>(Handle, address);
+		}
+	}
+}
