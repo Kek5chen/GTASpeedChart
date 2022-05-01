13ecee5e2e16e5a41b1b1f9f7aa6fa95eb840ed0
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-05-01 21:40:51 +0200

    Initial commit

---
+++ GTAChart/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GTAChart
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App
+	{
+	}
+}
---
+++ GTAChart/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace GTAChart
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public SeriesCollection SpeedSeries { get; set; }
+		
+		public MainWindow()
+		{
+			SpeedSeries = new SeriesCollection
+			{
+				new LineSeries
+				{
+					Title="Speeds1",
+					Values = new ChartValues<ObservableValue>
+					{
+						new ObservableValue(1),
+						new ObservableValue(2),
+						new ObservableValue(3),
+						new ObservableValue(2)
+					}
+				}
+			};
+			InitializeComponent();
+			SpeedSeries[0].Values.Add(new ObservableValue(4));
+			
+		}
+	}
+}
